addf29f646ab9ad51f26c286f1609e32e472c740
Darren Chamberlain <dlc@sevenroot.org>
2013-06-12 18:26:26 +0000

    Use the summary or body to generate a title if it's missing.

--- lib/Folderol.pm
+++ lib/Folderol.pm
@@ -200,10 +200,12 @@ sub parse {
 
         for my $entry ($p_feed->entries) {
             # Ensure there is a title
-            my $title = $entry->title;
+            my $title = $entry->title
+                     || substr($entry->summary->body, 0, 128)
+                     || substr($entry->content->body, 0, 128)
+                     || "(Untitled)";
             $title =~ s/^\s*//;
             $title =~ s/\s*$//;
-            $title ||= "(Untitled)";
 
             $self->db->save_entry(
                 FEED     => $feed_id,
